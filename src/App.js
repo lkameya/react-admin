f6490ba70201afe7f2968e606c6b87549c665684
Leonardo Kameya <leonardo.kameya@gmail.com>
2019-07-06 20:44:57 -0400

    Adding Dockerfile

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const getData = () => {
+    console.log('Teste');
+    fetch('/api/values')
+                    .then((response) => {
+                      response.json().then(data => {
+                        console.log(data);
+                      });
+                    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +28,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          {getData()}
         </a>
       </header>
     </div>
