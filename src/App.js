32dbac5ae54d7a663e1e8c8ff347248e46efc4e2
Leonardo Kameya <leonardo.kameya@gmail.com>
2019-07-06 21:53:38 -0400

    API values

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
   const getData = () => {
     console.log('Teste');
-    fetch('localhost:5000/api/values')
+    fetch('http://www.lkameya.com:5000/api/values')
                     .then((response) => {
                       response.json().then(data => {
                         console.log(data);
